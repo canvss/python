2d87ac014eca53887e5b23188ff3d54c7096f08b
sqlsafe <sqlsafe@qq.com>
2022-04-21 21:10:54 +0800

    文件操作

--- code/advanced/02-file_open_modify.py
+++ code/advanced/02-file_open_modify.py
@@ -9,6 +9,9 @@ import os
 
 
 with open('user.txt', mode='rt', encoding='utf-8')as rf,\
-    open('user.txt.swap', mode = 'wt', encoding='utf-8')as wf:
+    open('.user.txt.swap', mode = 'wt', encoding='utf-8')as wf:
     for line in rf:
         wf.write(line.replace('jack','tom'))
+
+os.remove('user.txt')
+os.rename('.user.txt.swap','user.txt')
